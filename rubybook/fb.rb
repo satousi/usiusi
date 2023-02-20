cf8d60ee57a69d78265d7c5541ed27d12e8fa668
satousi <usitatu@turs.xsrv.jp>
2023-02-20 23:15:04 +0900

    0220ロボコップカレンダー手直し

--- rubybook/fb.rb
+++ rubybook/fb.rb
@@ -1,10 +1,4 @@
-#1から20までの数をプリントするプログラムを書け。
-#ただし3の倍数のときは数の代わりに｢Fizz｣と、
-#5の倍数のときは｢Buzz｣とプリントし、
-#3と5両方の倍数の場合には｢FizzBuzz｣とプリントすること。
-
-
-(1..20).each do |x|
+def fizzbuzz(x)
   if x % 3 == 0 && x % 5 == 0
     puts "FizzBuzz"
   elsif x % 3 == 0
@@ -13,8 +7,10 @@
     puts "Buzz"
   else
     puts x
-  end  
+  end
 end
 
-
+(1..20).each do |x|
+  fizzbuzz(x)
+end
 
