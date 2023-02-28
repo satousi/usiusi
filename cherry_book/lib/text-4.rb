0fe9098593f9cd40187d8beb105e549b2effc4f3
satousi <usitatu@turs.xsrv.jp>
2023-02-28 10:09:58 +0900

    0227四章例題

--- cherry_book/lib/text-4.rb
+++ cherry_book/lib/text-4.rb
@@ -1,24 +1,5 @@
-numbers = [1, 2, 3, 4]
+numbers = []
 
-sum = 0
-numbers.each do |n|
-  sum_value = n.even? ? n*10:n
-  sum += sum_value
-end
+(1..10).step(2){|n| numbers << n}
 
-p sum
-# 条件 ? 式１ : 式２
-num = [1,2,3,4,5,]
-sum1 = 0
-num.each do |n|
-  if n.odd?
-    sum1 += n
-  end
-end
-p sum1
-
-nom = [1,2,3,4,5]
-sum2 = 0
-nom.each {|n|
-  sum2 += n}
-p sum2
+p numbers
